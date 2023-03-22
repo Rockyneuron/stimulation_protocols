42e75e358357ecc1543ccaaa4ab436cd6aa537b6
rockyneuron <ajvalpe@gmail.com>
2023-03-22 14:54:12 +0100

    [st] debut test to test time periods

--- pupil_labs/screen_stimulus_presentation/commons.py
+++ pupil_labs/screen_stimulus_presentation/commons.py
@@ -95,3 +95,26 @@ def getActualFrameRate(frames=1000,monitor=1):
     print('corresponding to a framerate of', round(1 / np.average(durations), 3), 'Hz')
     print('60 frames on your monitor takes', round(np.average(durations) * 60 * 1000, 3), 'ms')
     print('shortest duration was ', round(min(durations) * 1000, 3), 'ms and longest duration was ', round(max(durations) * 1000, 3), 'ms')
+
+
+def TicTocGenerator():
+    # Generator that returns time differences
+    ti = 0           # initial time
+    tf = time.time() # final time
+    while True:
+        ti = tf
+        tf = time.time()
+        yield tf-ti # returns the time difference
+
+TicToc = TicTocGenerator() # create an instance of the TicTocGen generator
+
+# This will be the main function through which we define both tic() and toc()
+def toc(tempBool=True):
+    # Prints the time difference yielded by generator instance TicToc
+    tempTimeInterval = next(TicToc)
+    if tempBool:
+        print( "Elapsed time: %f seconds.\n" %tempTimeInterval )
+
+def tic():
+    # Records a time in TicToc, marks the beginning of a time interval
+    toc(False)
